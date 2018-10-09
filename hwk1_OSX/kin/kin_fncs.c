177e1cd4d9df18396f2f0cd3b4475cf6731eabd7
Don Kuruppu <chalangakuruppu@gmail.com>
2018-10-09 09:29:04 -0500

    Coded forward kinematics.

--- hwk1_OSX/kin/kin_fncs.c
+++ hwk1_OSX/kin/kin_fncs.c
@@ -78,6 +78,7 @@ void MatrixMultiplication(double **matrixA, double **matrixB)
     // Copy B into C
     MatrixCopying(matrixB, matrixC);
     
+
     // Do the multiplication
     int j, k;
     for(i = 0; i < MATRIX_DIMENSION; i++)
@@ -94,6 +95,7 @@ void MatrixMultiplication(double **matrixA, double **matrixB)
             matrixB[i][j] = addResult;
         }
     }
+    
     
     // Free up C
     for(i = 0; i < MATRIX_DIMENSION; i++)
@@ -113,10 +115,9 @@ fwd_kin(theta, x)
 double *theta;
 double x[3];
 {
-    printf("\n\n");
-    
     int i;
     
+    // Initializing the two matrices
     double **matrixOne = (double **)malloc(MATRIX_DIMENSION * sizeof(double *));
     double **matrixTwo = (double **)malloc(MATRIX_DIMENSION * sizeof(double *));
     
@@ -126,20 +127,182 @@ double x[3];
         matrixTwo[i] = (double *)calloc(MATRIX_DIMENSION, sizeof(double));
     }
     
+
+    // Multiplying Dz(0.04x)Dx(0.15) * Ry(theta[3])
     matrixOne[0][0] = 1.0;
-    matrixOne[0][1] = 2.0;
-    matrixOne[1][0] = 3.0;
-    matrixOne[1][1] = 4.0;
+    matrixOne[0][1] = 0.0;
+    matrixOne[0][2] = 0.0;
+    matrixOne[0][3] = 0.15;
+    matrixOne[1][0] = 0.0;
+    matrixOne[1][1] = 1.0;
+    matrixOne[1][2] = 0.0;
+    matrixOne[1][3] = 0.0;
+    matrixOne[2][0] = 0.0;
+    matrixOne[2][1] = 0.0;
+    matrixOne[2][2] = 1.0;
+    matrixOne[2][3] = 0.04;
+    matrixOne[3][0] = 0.0;
+    matrixOne[3][1] = 0.0;
+    matrixOne[3][2] = 0.0;
+    matrixOne[3][3] = 1.0;
+
+    matrixTwo[0][0] = cos(theta[3]);
+    matrixTwo[0][1] = 0.0;
+    matrixTwo[0][2] = sin(theta[3]);
+    matrixTwo[0][3] = 0.0;
+    matrixTwo[1][0] = 0.0;
+    matrixTwo[1][1] = 1.0;
+    matrixTwo[1][2] = 0.0;
+    matrixTwo[1][3] = 0.0;
+    matrixTwo[2][0] = -1.0 * sin(theta[3]);
+    matrixTwo[2][1] = 0.0;
+    matrixTwo[2][2] = cos(theta[3]);
+    matrixTwo[2][3] = 0.0;
+    matrixTwo[3][0] = 0.0;
+    matrixTwo[3][1] = 0.0;
+    matrixTwo[3][2] = 0.0;
+    matrixTwo[3][3] = 1.0;
+
+    MatrixMultiplication(matrixOne, matrixTwo);
+    printf("Multiplying Dz(0.04x)Dx(0.15) * Ry(theta[3])\n");
+    PrintMatrix(matrixTwo);
     
-    matrixTwo[0][0] = 5.0;
-    matrixTwo[0][1] = 6.0;
-    matrixTwo[1][0] = 7.0;
-    matrixTwo[1][1] = 8.0;
+    // Multiplying Ry(theta[2]) * result_above
+    matrixOne[0][0] = cos(theta[2]);;
+    matrixOne[0][1] = 0.0;
+    matrixOne[0][2] = sin(theta[2]);
+    matrixOne[0][3] = 0.0;
+    matrixOne[1][0] = 0.0;
+    matrixOne[1][1] = 1.0;
+    matrixOne[1][2] = 0.0;
+    matrixOne[1][3] = 0.0;
+    matrixOne[2][0] = -1.0 * sin(theta[2]);
+    matrixOne[2][1] = 0.0;
+    matrixOne[2][2] = cos(theta[2]);
+    matrixOne[2][3] = 0.0;
+    matrixOne[3][0] = 0.0;
+    matrixOne[3][1] = 0.0;
+    matrixOne[3][2] = 0.0;
+    matrixOne[3][3] = 1.0;
+    
+    MatrixMultiplication(matrixOne, matrixTwo);
+    printf("Multiplying Ry(theta[2]) * result_above\n");
+    PrintMatrix(matrixTwo);
+    
+    // Multiplying Dy(0.04)Dx(0.2) * result_above
+    matrixOne[0][0] = 1.0;
+    matrixOne[0][1] = 0.0;
+    matrixOne[0][2] = 0.0;
+    matrixOne[0][3] = 0.2;
+    matrixOne[1][0] = 0.0;
+    matrixOne[1][1] = 1.0;
+    matrixOne[1][2] = 0.0;
+    matrixOne[1][3] = 0.04;
+    matrixOne[2][0] = 0.0;
+    matrixOne[2][1] = 0.0;
+    matrixOne[2][2] = 1.0;
+    matrixOne[2][3] = 0.0;
+    matrixOne[3][0] = 0.0;
+    matrixOne[3][1] = 0.0;
+    matrixOne[3][2] = 0.0;
+    matrixOne[3][3] = 1.0;
+    
+    MatrixMultiplication(matrixOne, matrixTwo);
+    printf("Multiplying Dy(0.04)Dx(0.2) * result_above\n");
+    PrintMatrix(matrixTwo);
+    
+    // Multiplying Ry(theta[1]) * result_above
+    matrixOne[0][0] = cos(theta[1]);
+    matrixOne[0][1] = 0.0;
+    matrixOne[0][2] = sin(theta[1]);
+    matrixOne[0][3] = 0.0;
+    matrixOne[1][0] = 0.0;
+    matrixOne[1][1] = 1.0;
+    matrixOne[1][2] = 0.0;
+    matrixOne[1][3] = 0.0;
+    matrixOne[2][0] = -1.0 * sin(theta[1]);
+    matrixOne[2][1] = 0.0;
+    matrixOne[2][2] = cos(theta[1]);
+    matrixOne[2][3] = 0.0;
+    matrixOne[3][0] = 0.0;
+    matrixOne[3][1] = 0.0;
+    matrixOne[3][2] = 0.0;
+    matrixOne[3][3] = 1.0;
+    
+    MatrixMultiplication(matrixOne, matrixTwo);
+    printf("Multiplying Ry(theta[1]) * result_above\n");
+    PrintMatrix(matrixTwo);
+    
+    // Multiplying Dx(0.2) * result_above
+    matrixOne[0][0] = 1.0;
+    matrixOne[0][1] = 0.0;
+    matrixOne[0][2] = 0.0;
+    matrixOne[0][3] = 0.2;
+    matrixOne[1][0] = 0.0;
+    matrixOne[1][1] = 1.0;
+    matrixOne[1][2] = 0.0;
+    matrixOne[1][3] = 0.0;
+    matrixOne[2][0] = 0.0;
+    matrixOne[2][1] = 0.0;
+    matrixOne[2][2] = 1.0;
+    matrixOne[2][3] = 0.0;
+    matrixOne[3][0] = 0.0;
+    matrixOne[3][1] = 0.0;
+    matrixOne[3][2] = 0.0;
+    matrixOne[3][3] = 1.0;
     
     MatrixMultiplication(matrixOne, matrixTwo);
     
-    PrintMatrix(matrixTwo);
     
+    // Multiplying Rz(theta[0]) * result_above
+    matrixOne[0][0] = cos(theta[0]);
+    matrixOne[0][1] = -1.0 * sin(theta[0]);
+    matrixOne[0][2] = 0.0;
+    matrixOne[0][3] = 0.0;
+    matrixOne[1][0] = sin(theta[0]);
+    matrixOne[1][1] = cos(theta[0]);
+    matrixOne[1][2] = 0.0;
+    matrixOne[1][3] = 0.0;
+    matrixOne[2][0] = 0.0;
+    matrixOne[2][1] = 0.0;
+    matrixOne[2][2] = 1.0;
+    matrixOne[2][3] = 0.0;
+    matrixOne[3][0] = 0.0;
+    matrixOne[3][1] = 0.0;
+    matrixOne[3][2] = 0.0;
+    matrixOne[3][3] = 1.0;
+    
+    MatrixMultiplication(matrixOne, matrixTwo);
+    
+    
+    // Multiplying Dz(0.25) * result_above
+    matrixOne[0][0] = 1.0;
+    matrixOne[0][1] = 0.0;
+    matrixOne[0][2] = 0.0;
+    matrixOne[0][3] = 0.0;
+    matrixOne[1][0] = 0.0;
+    matrixOne[1][1] = 1.0;
+    matrixOne[1][2] = 0.0;
+    matrixOne[1][3] = 0.0;
+    matrixOne[2][0] = 0.0;
+    matrixOne[2][1] = 0.0;
+    matrixOne[2][2] = 1.0;
+    matrixOne[2][3] = 0.25;
+    matrixOne[3][0] = 0.0;
+    matrixOne[3][1] = 0.0;
+    matrixOne[3][2] = 0.0;
+    matrixOne[3][3] = 1.0;
+    
+    MatrixMultiplication(matrixOne, matrixTwo);
+    
+    
+    // Extracting postion values
+    x[0] = matrixTwo[0][3];
+    x[1] = matrixTwo[1][3];
+    x[2] = matrixTwo[2][3];
+    
+    
+    // Freeing up the two matrices
     for(i = 0; i < MATRIX_DIMENSION; i++)
     {
         free(matrixOne[i]);
